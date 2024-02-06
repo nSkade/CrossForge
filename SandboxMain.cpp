57ad3c96bd0401779b21374ce40dab94e40ae6e9
Niclas Meyer <nicmcmey@gmail.com>
2024-02-06 12:05:16 +0100

    removed noise

--- SandboxMain.cpp
+++ SandboxMain.cpp
@@ -64,19 +64,6 @@ using namespace Eigen;
 
 ActiveScene* pScene = nullptr;
 
-std::string exec(const char* cmd) {
-    std::array<char, 128> buffer;
-    std::string result;
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-    if (!pipe) {
-        throw std::runtime_error("popen() failed!");
-    }
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-        result += buffer.data();
-    }
-    return result;
-}
-
 void mainLoop(void *pArg) {
 	static_cast<ActiveScene*>(pArg)->mainLoop();
 }//mainLoop
